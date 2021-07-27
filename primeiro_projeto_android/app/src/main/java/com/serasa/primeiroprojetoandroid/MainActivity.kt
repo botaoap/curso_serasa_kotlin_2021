44c1a217411409b0700424bb007488645f5c9acb
botaoap <gabrielbotaoap@gmail.com>
2021-07-27 14:46:10 -0300

    Ajustando classe main

--- primeiro_projeto_android/app/src/main/java/com/serasa/primeiroprojetoandroid/MainActivity.kt
+++ primeiro_projeto_android/app/src/main/java/com/serasa/primeiroprojetoandroid/MainActivity.kt
@@ -2,6 +2,7 @@ package com.serasa.primeiroprojetoandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.serasa.primeiroprojetoandroid.classes.ValidaLogin
 import com.serasa.primeiroprojetoandroid.classes.Veiculo
 
 class MainActivity : AppCompatActivity() {
@@ -9,18 +10,79 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // val veiculo = Veiculo("v6", "gasolina")
-        // chamar a variavel antes, usar este metodo
+        // classeVeiculo()
+        classeValidaLogin()
+
+    }
+
+    fun classeValidaLogin(){
+        var validacaoFalse = ValidaLogin().apply {
+            validaNome().let {
+                println(it)
+            }
+            validaEmail().let {
+                println(it)
+            }
+
+        }
+
+        var validacaoTrue = ValidaLogin(nome = "Jorge", email = "jorge@gmail.com").apply {
+            validaNome().let {
+                println(it)
+            }
+            validaEmail().let {
+                println(it)
+            }
+        }
+
+        var validacaoEmailFalse = ValidaLogin(nome = "Jorge").apply {
+            validaNome().let {
+                println(it)
+            }
+            validaEmail().let {
+                println(it)
+            }
+        }
+
+    }
+
+    fun classeVeiculo(){
+        // TODO: val veiculo = Veiculo("v6", "gasolina")
+        // TODO: chamar a variavel antes, usar este metodo
+        /*
         val veiculo = Veiculo(combustivel = "gasolina", motor = "v6")
+        val variavelDoVeiculo = Veiculo(combustivel = "gasolina", motor = "v6")
+        variavelDoVeiculo.verificarMotor()
+        variavelDoVeiculo.verificarCombustivel()
+        */
+        // TODO: USANDO METODOS DO KOTLIN(apply, let)
+        // TODO: Fazendo o mesmo code de cima em Kotlin
+        /*
+        Veiculo(combustivel = "diesel", motor="v6").apply {
+            verificarCombustivel()
+            verificarMotor()
+        }
+        */
 
-        veiculo.verificarMotor()
+        // TODO: atribuindo uma variavel
+        /*
+        val primeiroVeiculo = Veiculo().apply {
+            // let passa uma variavel
+            verificarMotorBooleanValidaFacil().let {
+                println(it)
+            }
+        }
+        */
 
-        println(veiculo.combustivel)
+        // TODO: problema nos construtores por causa do tipo da variavel do motor "val"
+        /*
+        val segundoVeiculo = Veiculo(motor="v8").apply {
+            verificarMotorBooleanValidaFacil().let {
+                println(it)
+            }
+        }
+        */
 
-        veiculo.combustivel = "diesel"
-
-        println(veiculo.combustivel)
-
-
+        // TODO: METODO TRADICIONAL
     }
 }
