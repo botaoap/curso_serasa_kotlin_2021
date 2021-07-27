501784257ecc3e0aa83dda7c31a3ea6d132ee0ed
botaoap <gabrielbotaoap@gmail.com>
2021-07-27 14:46:25 -0300

    Metodo de validar

--- primeiro_projeto_android/app/src/main/java/com/serasa/primeiroprojetoandroid/classes/ValidaLogin.kt
+++ primeiro_projeto_android/app/src/main/java/com/serasa/primeiroprojetoandroid/classes/ValidaLogin.kt
@@ -1,4 +1,19 @@
 package com.serasa.primeiroprojetoandroid.classes
 
-class ValidaLogin {
+class ValidaLogin(
+    var nome: String,
+    var email: String
+) {
+
+    constructor(): this("","")
+
+    constructor(nome: String): this(nome,"")
+
+    fun validaEmail(): Boolean{
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    fun validaNome(): Boolean{
+        return nome.isNotBlank()
+    }
 }
