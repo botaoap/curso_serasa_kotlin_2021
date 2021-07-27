c62f3e79c84f303b5824734d7a3ef59c94eadbd6
botaoap <gabrielbotaoap@gmail.com>
2021-07-27 14:46:46 -0300

    Classe de veiculo

--- primeiro_projeto_android/app/src/main/java/com/serasa/primeiroprojetoandroid/classes/Veiculo.kt
+++ primeiro_projeto_android/app/src/main/java/com/serasa/primeiroprojetoandroid/classes/Veiculo.kt
@@ -1,12 +1,34 @@
 package com.serasa.primeiroprojetoandroid.classes
 
 class Veiculo(
-    private val motor: String,
+    var motor: String,
     var combustivel: String,
 ){
 
+    constructor() : this("","")
+
+    constructor(motor: String) : this(motor, "")
+
+    // TODO: usando if para validar
+    fun verificarMotorBoolean(): Boolean {
+        if (motor.isNotEmpty() && motor.length >= 2)
+            return true
+        return false
+    }
+
+    // TODO: a mesma validaçao sem if
+    fun verificarMotorBooleanValidaFacil(): Boolean {
+        return motor.isNotEmpty() && motor.length >= 2
+    }
+
+
+
     fun verificarMotor() {
         println(motor)
+    }
+
+    fun verificarCombustivel() {
+        println(combustivel)
     }
 
 }
