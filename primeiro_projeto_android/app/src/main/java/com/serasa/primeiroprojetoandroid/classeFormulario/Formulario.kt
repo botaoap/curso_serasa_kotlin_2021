103218eb45697b11022c42d7778467a5a37f62c6
botaoap <gabrielbotaoap@gmail.com>
2021-07-28 14:10:07 -0300

    Recgra de Negocio simples do Form

--- primeiro_projeto_android/app/src/main/java/com/serasa/primeiroprojetoandroid/classeFormulario/Formulario.kt
+++ primeiro_projeto_android/app/src/main/java/com/serasa/primeiroprojetoandroid/classeFormulario/Formulario.kt
@@ -6,4 +6,22 @@ class Formulario(
     var celular: String?
 ) {
     constructor(): this(null,null,null)
+
+    // TODO: Funcao de tratamento do formulario
+    fun checkFields(): Boolean {
+        return checkNome() && checkEmail() && checkCelular()
+    }
+
+    fun checkNome(): Boolean {
+        return !nome.isNullOrEmpty()
+    }
+
+    fun checkEmail(): Boolean {
+        return !email.isNullOrEmpty()
+    }
+
+    fun checkCelular(): Boolean {
+        return !celular.isNullOrEmpty()
+    }
+
 }
