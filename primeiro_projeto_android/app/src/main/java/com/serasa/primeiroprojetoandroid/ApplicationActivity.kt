5dd9de52bf07e7e70a713910994b259ed877d886
botaoap <gabrielbotaoap@gmail.com>
2021-07-28 15:49:01 -0300

    Criando outra Activity para chamar ela pela MainActivity

---
+++ primeiro_projeto_android/app/src/main/java/com/serasa/primeiroprojetoandroid/ApplicationActivity.kt
@@ -0,0 +1,11 @@
+package com.serasa.primeiroprojetoandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ApplicationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_application)
+    }
+}
