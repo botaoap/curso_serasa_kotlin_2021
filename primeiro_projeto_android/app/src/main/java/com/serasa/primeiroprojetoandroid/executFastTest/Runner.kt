ebd73b16cb4fef6425dc5c4b480afb7765abb28e
botaoap <gabrielbotaoap@gmail.com>
2021-07-29 16:30:01 -0300

    Exemplos de Ifternario e When

--- primeiro_projeto_android/app/src/main/java/com/serasa/primeiroprojetoandroid/executFastTest/Runner.kt
+++ primeiro_projeto_android/app/src/main/java/com/serasa/primeiroprojetoandroid/executFastTest/Runner.kt
@@ -6,19 +6,33 @@ import com.serasa.primeiroprojetoandroid.usingHeranca.Cobra
 import com.serasa.primeiroprojetoandroid.usingInterface.AutoFalante
 
 fun main() {
+    // TODO: Usando ifternario no Kotlin
+    val num = 11
+    val retorno = if (num > 9) "Numero é maior q 9" else if (num < 6) "Menor que 6" else "maior q 6 menor q 9"
 
-    val cachorro = Cachorro()
-    val cobra = Cobra()
-    val veiculo = Veiculo()
+    println(retorno)
 
+    // TODO: Usando when, é parecido com um switch case
+    val retornoWhen = when(num) {
+        in 0..4 -> "Regular, Nota: ${num}"
+        in 5..7 -> "Bom, Nota: ${num}"
+        !in 8..10 -> "Otimo, Nota: ${num}"
+        else -> "Nenhum numero informado"
+    }
+
+    print(retornoWhen)
+//    val cachorro = Cachorro()
+//    val cobra = Cobra()
+//    val veiculo = Veiculo()
+//
     // TODO: AutoFalante le cada interface
-    val autoFalante = AutoFalante()
-
-    autoFalante.reproduzirSom(cachorro)
-    autoFalante.reproduzirSom(cobra)
-    autoFalante.reproduzirSom(veiculo)
-
-    autoFalante.reproduzirMovimento(cachorro)
-    autoFalante.reproduzirMovimento(cobra)
-    autoFalante.reproduzirMovimento(veiculo)
+//    val autoFalante = AutoFalante()
+//
+//    autoFalante.reproduzirSom(cachorro)
+//    autoFalante.reproduzirSom(cobra)
+//    autoFalante.reproduzirSom(veiculo)
+//
+//    autoFalante.reproduzirMovimento(cachorro)
+//    autoFalante.reproduzirMovimento(cobra)
+//    autoFalante.reproduzirMovimento(veiculo)
 }
