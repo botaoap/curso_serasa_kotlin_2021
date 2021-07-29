1b81447436ce64bf8f04d1b2846e50f206cff952
botaoap <gabrielbotaoap@gmail.com>
2021-07-29 09:10:22 -0300

    Testando interfaces criadas

--- primeiro_projeto_android/app/src/main/java/com/serasa/primeiroprojetoandroid/executFastTest/Runner.kt
+++ primeiro_projeto_android/app/src/main/java/com/serasa/primeiroprojetoandroid/executFastTest/Runner.kt
@@ -1,2 +1,19 @@
 package com.serasa.primeiroprojetoandroid.executFastTest
 
+import com.serasa.primeiroprojetoandroid.classes.Veiculo
+import com.serasa.primeiroprojetoandroid.usingHeranca.Cachorro
+import com.serasa.primeiroprojetoandroid.usingHeranca.Cobra
+import com.serasa.primeiroprojetoandroid.usingInterface.AutoFalante
+
+fun main() {
+
+    val cachorro = Cachorro()
+    val cobra = Cobra()
+    val veiculo = Veiculo()
+
+    val autoFalante = AutoFalante()
+
+    autoFalante.reproduzirSom(cachorro)
+    autoFalante.reproduzirSom(cobra)
+    autoFalante.reproduzirSom(veiculo)
+}
