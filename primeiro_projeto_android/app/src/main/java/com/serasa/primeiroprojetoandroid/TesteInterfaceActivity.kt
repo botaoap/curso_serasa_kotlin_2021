9e455eb426181d90d48e72492f8a3736849ab81b
botaoap <gabrielbotaoap@gmail.com>
2021-07-29 15:48:44 -0300

    Chamando dois Buttons e lendo Activity

--- primeiro_projeto_android/app/src/main/java/com/serasa/primeiroprojetoandroid/TesteInterfaceActivity.kt
+++ primeiro_projeto_android/app/src/main/java/com/serasa/primeiroprojetoandroid/TesteInterfaceActivity.kt
@@ -2,10 +2,69 @@ package com.serasa.primeiroprojetoandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.constraintlayout.solver.state.State
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
 
-class TesteInterfaceActivity : AppCompatActivity() {
+class TesteInterfaceActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_teste_interface)
+
+        chamandoButton()
+
     }
+
+    fun chamandoButton() {
+        // TODO: Chamando o botao normal
+//        val buttonTeste1 = findViewById<Button>(R.id.buttonTeste1)
+//        buttonTeste1.setOnClickListener(this)
+//
+//        val buttonTeste2 = findViewById<Button>(R.id.buttonTeste2)
+//        buttonTeste2.setOnClickListener(this)
+        // TODO: Usando dois botoes com .apply
+        findViewById<Button>(R.id.buttonTeste1).apply {
+            // TODO: fica em um loop para escutar o button da View onde esta
+            setOnClickListener(this@TesteInterfaceActivity)
+        }
+        findViewById<Button>(R.id.buttonTeste2).apply {
+            setOnClickListener(this@TesteInterfaceActivity)
+        }
+
+        findViewById<ConstraintLayout>(R.id.activityTelaTeste).apply {
+            setOnClickListener(this@TesteInterfaceActivity)
+        }
+    }
+
+    override fun onClick(view: View?) {
+        view?.let {
+         when(it.id) {
+             R.id.buttonTeste1 -> {
+                 Snackbar.make(it, getString(R.string.message_teste1),
+                     Snackbar.LENGTH_LONG).show()
+             }
+             R.id.buttonTeste2 -> {
+                 Snackbar.make(it, getString(R.string.message_teste2),
+                     Snackbar.LENGTH_LONG).show()
+             }
+             R.id.activityTelaTeste -> {
+                 Snackbar.make(it,getString(R.string.message_testeTela),
+                     Snackbar.LENGTH_LONG).show()
+             }
+         }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
