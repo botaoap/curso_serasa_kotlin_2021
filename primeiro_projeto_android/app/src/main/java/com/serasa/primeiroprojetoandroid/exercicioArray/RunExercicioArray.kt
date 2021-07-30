6fb72b24c5f27dfcd58717bface253dbcca9748a
botaoap <gabrielbotaoap@gmail.com>
2021-07-30 14:38:06 -0300

    Exercicio de Arrays

---
+++ primeiro_projeto_android/app/src/main/java/com/serasa/primeiroprojetoandroid/exercicioArray/RunExercicioArray.kt
@@ -0,0 +1,11 @@
+package com.serasa.primeiroprojetoandroid.exercicioArray
+
+fun main() {
+
+    val arrayDeNome = arrayOf("Matheus", "Gabriel", "Arthur", "Andrei", "Giovanni", "Vinicius",
+        "Gabriela", "Felipe", "Edson", "Danilo", "Jaison")
+    
+    arrayDeNome.sorted().forEach {
+        println(it)
+    }
+}
