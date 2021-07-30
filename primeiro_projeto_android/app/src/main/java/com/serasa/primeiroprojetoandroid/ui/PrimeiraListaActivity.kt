484f859460f894cb2bf24c67874cf571fbd8807e
botaoap <gabrielbotaoap@gmail.com>
2021-07-30 17:20:52 -0300

    Manipulando a listsa para clicar em cada posicao

--- primeiro_projeto_android/app/src/main/java/com/serasa/primeiroprojetoandroid/ui/PrimeiraListaActivity.kt
+++ primeiro_projeto_android/app/src/main/java/com/serasa/primeiroprojetoandroid/ui/PrimeiraListaActivity.kt
@@ -2,8 +2,10 @@ package com.serasa.primeiroprojetoandroid.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.serasa.primeiroprojetoandroid.R
 import com.serasa.primeiroprojetoandroid.usingAdapter.AdapterListaNomes
 
@@ -21,12 +23,17 @@ class PrimeiraListaActivity : AppCompatActivity() {
 
     fun chamandoRecycleView() {
 
-        val arrayNomesTeste = arrayListOf("Arthur", "Gabriel", "Sergio", "Heloise")
+        val arrayNomesTeste = arrayListOf("Arthur", "Gabriel", "Sergio", "Heloise", "Arthur", "Gabriel", "Sergio", "Heloise")
         recycleViewNomes = findViewById(R.id.recycleViewLista)
         // TODO: Usando o adapter, é a peça fundamental para funcionar
-        recycleViewNomes.adapter = AdapterListaNomes(arrayNomesTeste)
+        recycleViewNomes.adapter = AdapterListaNomes(arrayNomesTeste) { nome, id ->
+            Snackbar.make(recycleViewNomes, "Id: $id | Nome: $nome ", Snackbar.LENGTH_LONG).show()
+        }
         // TODO: Quem sera o motor e o layoutManager do adapter
-        recycleViewNomes.layoutManager = LinearLayoutManager(this)
+        recycleViewNomes.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        // TODO: Layout para deslizar horizontal
+        // recycleViewNomes.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
     }
 
 }
--- primeiro_projeto_android/app/src/main/java/com/serasa/primeiroprojetoandroid/usingAdapter/AdapterListaNomes.kt
+++ primeiro_projeto_android/app/src/main/java/com/serasa/primeiroprojetoandroid/usingAdapter/AdapterListaNomes.kt
@@ -8,7 +8,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.serasa.primeiroprojetoandroid.R
 import org.w3c.dom.Text
 
-class AdapterListaNomes(val arrayDeNomes: ArrayList<String>) : RecyclerView.Adapter<ViewHolderNomes>() {
+class AdapterListaNomes(
+    val arrayDeNomes: ArrayList<String>,
+    val  onClick: (String, Int) -> Unit
+    ) : RecyclerView.Adapter<ViewHolderNomes>() {
 
     // TODO: Rsponsavel por definir o layout
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderNomes {
@@ -21,6 +24,9 @@ class AdapterListaNomes(val arrayDeNomes: ArrayList<String>) : RecyclerView.Adap
     // TODO: Responsavel por preencher os campos
     override fun onBindViewHolder(holder: ViewHolderNomes, position: Int) {
         holder.textViewNome.text = arrayDeNomes[position]
+        holder.itemView.setOnClickListener{
+            onClick(arrayDeNomes[position], position)
+        }
     }
 
     // TODO: Responsavel por contar quantos itens ira retornar
