78a22b42bf6ecba4f7da5462e770014e521518c3
botaoap <gabrielbotaoap@gmail.com>
2021-08-02 10:17:26 -0300

    Preenchendo al lista de cars

--- projeto_segunda_semana/app/src/main/java/com/serasa/projeto_segunda_semana/ui/MainActivity.kt
+++ projeto_segunda_semana/app/src/main/java/com/serasa/projeto_segunda_semana/ui/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.serasa.projeto_segunda_semana.R
+import com.serasa.projeto_segunda_semana.model.Car
+import com.serasa.projeto_segunda_semana.usingAdapterForList.AdapterRecyclerView
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,10 +22,20 @@ class MainActivity : AppCompatActivity() {
 
     fun criandolistaRecyclerView() {
 
+        // TODO: criando array de cars
+        val listOfCars = listOf<Car>(
+            Car("Mercedes", "AMG A 45 S",2019),
+            Car("Mercedes", "AMG Class S",2021),
+            Car("BMW", "345i",2018),
+            Car("Chevrolet", "Camaro",2017),
+            Car("Dodge", "Muscle Car",1999),
+        )
+
+
         carReclycerView = findViewById(R.id.carsRecyclerView)
         carReclycerView.layoutManager = LinearLayoutManager(this,
             LinearLayoutManager.VERTICAL, false)
-
+        carReclycerView.adapter = AdapterRecyclerView(listOfCars)
 
 
     }
