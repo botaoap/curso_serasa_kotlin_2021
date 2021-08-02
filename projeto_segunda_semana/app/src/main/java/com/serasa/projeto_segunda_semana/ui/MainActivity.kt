fc2c6533dcda17d9c5f997321b7a50288ddde9ca
botaoap <gabrielbotaoap@gmail.com>
2021-08-02 09:09:43 -0300

    Manipulando Lista

--- projeto_segunda_semana/app/src/main/java/com/serasa/projeto_segunda_semana/ui/MainActivity.kt
+++ projeto_segunda_semana/app/src/main/java/com/serasa/projeto_segunda_semana/ui/MainActivity.kt
@@ -1,11 +1,24 @@
-package com.serasa.projeto_segunda_semana
+package com.serasa.projeto_segunda_semana.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.serasa.projeto_segunda_semana.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var carReclycerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        criandolistaRecyclerView()
+
+    }
+
+    fun criandolistaRecyclerView() {
+
+        carReclycerView = findViewById(R.id.carsRecyclerView)
     }
 }
