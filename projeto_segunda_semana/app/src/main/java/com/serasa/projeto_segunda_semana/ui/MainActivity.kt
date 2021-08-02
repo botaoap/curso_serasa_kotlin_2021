556f388b12039ccc73d149da1a3f8fddbdc8c7bb
botaoap <gabrielbotaoap@gmail.com>
2021-08-02 09:19:18 -0300

    Manipulando RecyclerView para Lista

--- projeto_segunda_semana/app/src/main/java/com/serasa/projeto_segunda_semana/ui/MainActivity.kt
+++ projeto_segunda_semana/app/src/main/java/com/serasa/projeto_segunda_semana/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.serasa.projeto_segunda_semana.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.serasa.projeto_segunda_semana.R
 
@@ -20,5 +21,10 @@ class MainActivity : AppCompatActivity() {
     fun criandolistaRecyclerView() {
 
         carReclycerView = findViewById(R.id.carsRecyclerView)
+        carReclycerView.layoutManager = LinearLayoutManager(this,
+            LinearLayoutManager.VERTICAL, false)
+
+
+
     }
 }
