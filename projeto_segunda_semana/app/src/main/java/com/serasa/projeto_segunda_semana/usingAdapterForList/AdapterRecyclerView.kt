84af27d2fb4f13e0c71185513dc3e7333efb3428
botaoap <gabrielbotaoap@gmail.com>
2021-08-02 09:19:29 -0300

    Montando adapter para Lista

--- projeto_segunda_semana/app/src/main/java/com/serasa/projeto_segunda_semana/usingAdapterForList/AdapterRecyclerView.kt
+++ projeto_segunda_semana/app/src/main/java/com/serasa/projeto_segunda_semana/usingAdapterForList/AdapterRecyclerView.kt
@@ -1,4 +1,24 @@
 package com.serasa.projeto_segunda_semana.usingAdapterForList
 
-class AdapterRecyclerView {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class AdapterRecyclerView: RecyclerView.Adapter<ItemCarViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemCarViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ItemCarViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
+
+class ItemCarViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
 }
