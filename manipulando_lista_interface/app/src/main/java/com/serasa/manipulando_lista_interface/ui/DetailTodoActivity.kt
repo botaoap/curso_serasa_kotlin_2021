6903a3c12e8a2e4aea3b1c7c4b1051a55c77aa2a
botaoap <gabrielbotaoap@gmail.com>
2021-08-03 10:52:06 -0300

    Criando DetailActivity para pegar as informacoes da lista

---
+++ manipulando_lista_interface/app/src/main/java/com/serasa/manipulando_lista_interface/ui/DetailTodoActivity.kt
@@ -0,0 +1,42 @@
+package com.serasa.manipulando_lista_interface.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.serasa.manipulando_lista_interface.R
+import com.serasa.manipulando_lista_interface.model.ListTodo
+
+class DetailTodoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_todo2)
+        
+        val todo = intent.getSerializableExtra("parametro-todo") as? ListTodo
+
+        // TODO: 03/08/21 Faz um button de voltar no canto superior direito 
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        
+        // TODO: busca as informacoes necessarias da lista e mostra em outra tela em detalhe
+        todo?.let { 
+            findViewById<ImageView>(R.id.logoDetailImageView).apply {
+                Glide.with(context)
+                    .load(todo.logo.url)
+                    .placeholder(R.drawable.ic_baseline_error_24)
+                    .into(this)
+            }
+            findViewById<TextView>(R.id.titleDetailTextView).apply {
+                text = todo.title
+            }
+            findViewById<TextView>(R.id.descriptiomDetailTextView).apply {
+                text = todo.description
+            }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return super.onSupportNavigateUp()
+    }
+}
