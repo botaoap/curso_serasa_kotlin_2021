99f229cb7376a093dabbb9e6ce08ecd8423e48a1
botaoap <gabrielbotaoap@gmail.com>
2021-08-04 09:11:39 -0300

    Remoendo elemento

--- manipulando_lista_interface/app/src/main/java/com/serasa/manipulando_lista_interface/ui/SegundaActivity.kt
+++ manipulando_lista_interface/app/src/main/java/com/serasa/manipulando_lista_interface/ui/SegundaActivity.kt
@@ -2,11 +2,25 @@ package com.serasa.manipulando_lista_interface.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
 import com.serasa.manipulando_lista_interface.R
+import com.serasa.manipulando_lista_interface.singleton.ListaControle
 
 class SegundaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_segunda)
+
+        ListaControle.nomes.remove("Joao")
+    }
+
+    override fun onResume() {
+        println("ON RESUMO")
+
+        Snackbar.make(window.decorView,
+            "Lista com: ${ListaControle.nomes.size} elementos",
+            Snackbar.LENGTH_LONG).show()
+
+        super.onResume()
     }
 }
