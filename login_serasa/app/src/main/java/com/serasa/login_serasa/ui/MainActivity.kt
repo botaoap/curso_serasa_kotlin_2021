9640ff17a69f8edd454b2a1c009801dfbb028f75
botaoap <gabrielbotaoap@gmail.com>
2021-08-04 14:56:44 -0300

    Projeto de login Serasa

---
+++ login_serasa/app/src/main/java/com/serasa/login_serasa/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.serasa.login_serasa.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.serasa.login_serasa.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
