761ee36a33414e65c860bc68b871869c53b5a5ec
botaoap <gabrielbotaoap@gmail.com>
2021-08-04 10:32:24 -0300

    Classe Enum para passar URL

--- manipulando_lista_interface/app/src/main/java/com/serasa/manipulando_lista_interface/exercicioSingleton/model/LogoProduto.kt
+++ manipulando_lista_interface/app/src/main/java/com/serasa/manipulando_lista_interface/exercicioSingleton/model/LogoProduto.kt
@@ -1,4 +1,7 @@
 package com.serasa.manipulando_lista_interface.exercicioSingleton.model
 
-enum class LogoProduto {
+enum class LogoProduto(val url: String) {
+    PAO_DE_QUEIJO(""),
+    PAO_FRANCES(""),
+    PAO_INTEGRAL(""),
 }
