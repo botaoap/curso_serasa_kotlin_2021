6deb49f4737faab7b59fffd321e0ff48c34c422d
botaoap <gabrielbotaoap@gmail.com>
2021-08-04 10:32:59 -0300

    Criando Activity Exercicio Singleton

--- manipulando_lista_interface/app/src/main/java/com/serasa/manipulando_lista_interface/exercicioSingleton/ui/ExSingleton.kt
+++ manipulando_lista_interface/app/src/main/java/com/serasa/manipulando_lista_interface/exercicioSingleton/ui/ExSingleton.kt
@@ -2,10 +2,61 @@ package com.serasa.manipulando_lista_interface.exercicioSingleton.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.serasa.manipulando_lista_interface.R
+import com.serasa.manipulando_lista_interface.exercicioSingleton.adapter.AdapterSingleton
+import com.serasa.manipulando_lista_interface.exercicioSingleton.adapter.ClickableSingleton
+import com.serasa.manipulando_lista_interface.exercicioSingleton.model.ProdutoSingleton
+import com.serasa.manipulando_lista_interface.exercicioSingleton.singleton.ListaSingleton
 
-class ExSingleton : AppCompatActivity() {
+class ExSingleton : AppCompatActivity(), ClickableSingleton {
+
+    private lateinit var singletonRecyclerView : RecyclerView
+    private lateinit var adapter: AdapterSingleton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ex_singleton)
+
+        chamandoSingleton()
     }
+
+    fun chamandoSingleton(){
+
+        adapter = AdapterSingleton(this)
+
+        singletonRecyclerView = findViewById(R.id.RecycleViewItemListaSingleton)
+        singletonRecyclerView.layoutManager =  LinearLayoutManager(
+            this, LinearLayoutManager.VERTICAL, false
+        )
+        singletonRecyclerView.adapter = adapter
+
+
+    }
+
+    override fun onDelete(produtoSingleton: ProdutoSingleton) {
+        if (singletonRecyclerView.adapter is AdapterSingleton) {
+            (singletonRecyclerView.adapter as AdapterSingleton).remove(produtoSingleton)
+            atualizarListaSingleton()
+        }
+    }
+
+    override fun onResume() {
+        println("ON RESUME")
+
+        atualizarListaSingleton()
+
+        adapter.refresh()
+
+        super.onResume()
+    }
+
+    fun atualizarListaSingleton(){
+        findViewById<TextView>(R.id.TextViewQtdItemLista).apply {
+            text = "Qtd itens na lista: ${ListaSingleton.listaProdutos.size}"
+        }
+    }
+
 }
