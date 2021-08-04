828a19b9f2ef57119dcb2d96270bfce449737b04
botaoap <gabrielbotaoap@gmail.com>
2021-08-04 10:31:18 -0300

    Lista Singleton

--- manipulando_lista_interface/app/src/main/java/com/serasa/manipulando_lista_interface/exercicioSingleton/singleton/ListaSingleton.kt
+++ manipulando_lista_interface/app/src/main/java/com/serasa/manipulando_lista_interface/exercicioSingleton/singleton/ListaSingleton.kt
@@ -1,4 +1,14 @@
 package com.serasa.manipulando_lista_interface.exercicioSingleton.singleton
 
+import com.serasa.manipulando_lista_interface.exercicioSingleton.model.LogoProduto
+import com.serasa.manipulando_lista_interface.exercicioSingleton.model.ProdutoSingleton
+import java.util.*
+
 object ListaSingleton {
+
+    var listaProdutos = mutableListOf<ProdutoSingleton>(
+        ProdutoSingleton(UUID.randomUUID().leastSignificantBits,"Pao","Pao frances",LogoProduto.PAO_FRANCES),
+        ProdutoSingleton(UUID.randomUUID().leastSignificantBits,"Pao","Pao Integral",LogoProduto.PAO_INTEGRAL),
+        ProdutoSingleton(UUID.randomUUID().leastSignificantBits,"Pao de Queijo","Feito de queijo",LogoProduto.PAO_DE_QUEIJO),
+    )
 }
