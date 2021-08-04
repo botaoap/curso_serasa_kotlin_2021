a335e0bb1bdfe2dad45df209d172c5fa98bc3ac4
botaoap <gabrielbotaoap@gmail.com>
2021-08-04 11:53:10 -0300

    Concatenando com Extension

--- manipulando_lista_interface/app/src/main/java/com/serasa/manipulando_lista_interface/extensions/RunnerExtensions.kt
+++ manipulando_lista_interface/app/src/main/java/com/serasa/manipulando_lista_interface/extensions/RunnerExtensions.kt
@@ -1,2 +1,17 @@
 package com.serasa.manipulando_lista_interface.extensions
 
+fun main() {
+
+}
+
+// TODO: Criando extension para uma String
+
+fun String.concatNameWithCompany() : String {
+    return "$this - Proway"
+}
+
+
+// TODO: Criar extension para remover ponto e traco do CPF
+fun String.concatPontoTracoCpf() : String {
+    return "$this"
+}
