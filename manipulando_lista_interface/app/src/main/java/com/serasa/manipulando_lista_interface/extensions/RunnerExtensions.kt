69db72887b2dde381e9a41ece0fff05a259d8a15
botaoap <gabrielbotaoap@gmail.com>
2021-08-04 13:34:17 -0300

    Acionando ponto e traco no CPF com Regex

--- manipulando_lista_interface/app/src/main/java/com/serasa/manipulando_lista_interface/extensions/RunnerExtensions.kt
+++ manipulando_lista_interface/app/src/main/java/com/serasa/manipulando_lista_interface/extensions/RunnerExtensions.kt
@@ -1,9 +1,13 @@
 package com.serasa.manipulando_lista_interface.extensions
 
 fun main() {
-    val cpf = "084.000.000-44"
+    val cpfCerto = "084.000.000-44"
 
-    println(cpf.concatPontoTracoCpf())
+    println(cpfCerto.concatPontoTracoCpf())
+
+    val cpfErrado = "09823993988"
+
+    println(cpfErrado.adicionadoPontoTracoCpf())
 }
 
 // TODO: Criando extension para uma String
@@ -17,4 +21,12 @@ fun String.concatNameWithCompany() : String {
 fun String.concatPontoTracoCpf() : String {
 
     return replace(".","").replace("-","")
+}
+
+// TODO: Colocar os pontos e o traco no CPF
+fun String.adicionadoPontoTracoCpf() : String {
+    val padraoCpf = "([0-9]{3})([0-9]{3})([0-9]{3})([0-9]{2})".toRegex()
+    val padraoEscrito = "$1.$2.$3-$4"
+
+    return replace(padraoCpf, padraoEscrito)
 }
