64d81b725a7c4758107f1774830151f52784afd7
botaoap <gabrielbotaoap@gmail.com>
2021-08-04 11:10:20 -0300

    Inserindo Logo

--- manipulando_lista_interface/app/src/main/java/com/serasa/manipulando_lista_interface/exercicioSingleton/model/LogoProduto.kt
+++ manipulando_lista_interface/app/src/main/java/com/serasa/manipulando_lista_interface/exercicioSingleton/model/LogoProduto.kt
@@ -1,7 +1,7 @@
 package com.serasa.manipulando_lista_interface.exercicioSingleton.model
 
 enum class LogoProduto(val url: String) {
-    PAO_DE_QUEIJO(""),
-    PAO_FRANCES(""),
-    PAO_INTEGRAL(""),
+    PAO_DE_QUEIJO("https://statics.angeloni.com.br/super/files/produtos/3680714/3680714_1_zoom.jpg"),
+    PAO_FRANCES("https://hiperideal.vteximg.com.br/arquivos/ids/181563-1000-1000/82929.jpg?v=637248333767400000"),
+    PAO_INTEGRAL("https://www.receitas-sem-fronteiras.com/uploads/media/amant-7.jpg?1392829036"),
 }
