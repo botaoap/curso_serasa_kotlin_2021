c9e2d04bc33aa2f41334adce6da91899205e3d49
botaoap <gabrielbotaoap@gmail.com>
2021-08-04 10:31:38 -0300

    Interface para acoes de Button

--- manipulando_lista_interface/app/src/main/java/com/serasa/manipulando_lista_interface/exercicioSingleton/adapter/ClickableSingleton.kt
+++ manipulando_lista_interface/app/src/main/java/com/serasa/manipulando_lista_interface/exercicioSingleton/adapter/ClickableSingleton.kt
@@ -1,4 +1,8 @@
 package com.serasa.manipulando_lista_interface.exercicioSingleton.adapter
 
+import com.serasa.manipulando_lista_interface.exercicioSingleton.model.ProdutoSingleton
+
 interface ClickableSingleton {
+
+    fun onDelete(produtoSingleton: ProdutoSingleton)
 }
