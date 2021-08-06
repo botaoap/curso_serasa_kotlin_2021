e1880b4aeb522b659df18769dcfb66fcb2f1046d
botaoap <gabrielbotaoap@gmail.com>
2021-08-06 08:48:31 -0300

    Login Consumindo Api

---
+++ login_serasa/app/src/main/java/com/serasa/login_serasa/loginConsumindoApi/ui/LoginConsumindoApi.kt
@@ -0,0 +1,12 @@
+package com.serasa.login_serasa.loginConsumindoApi.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.serasa.login_serasa.R
+
+class LoginConsumindoApi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_consumindo_api)
+    }
+}
