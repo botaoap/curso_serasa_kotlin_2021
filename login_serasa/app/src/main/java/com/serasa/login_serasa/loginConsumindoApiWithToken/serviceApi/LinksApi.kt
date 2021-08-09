75948081a191439af01320712544c2ba5e451c80
botaoap <gabrielbotaoap@gmail.com>
2021-08-09 10:21:16 -0300

    Enum para links de Api

--- login_serasa/app/src/main/java/com/serasa/login_serasa/loginConsumindoApiWithToken/serviceApi/LinksApi.kt
+++ login_serasa/app/src/main/java/com/serasa/login_serasa/loginConsumindoApiWithToken/serviceApi/LinksApi.kt
@@ -1,4 +1,6 @@
 package com.serasa.login_serasa.loginConsumindoApiWithToken.serviceApi
 
-enum class LinksApi {
+enum class LinksApi(val url: String) {
+    FAKE_STORE_API("https://fakestoreapi.com"),
+    PARALLELUM("https://parallelum.com.br")
 }
