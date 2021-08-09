58230fe244345734eedd6f3f69eec56818ea11c8
botaoap <gabrielbotaoap@gmail.com>
2021-08-09 14:07:46 -0300

    Authentication

--- login_serasa/app/src/main/java/com/serasa/login_serasa/loginConsumindoApiWithToken/model/Auth.kt
+++ login_serasa/app/src/main/java/com/serasa/login_serasa/loginConsumindoApiWithToken/model/Auth.kt
@@ -4,6 +4,15 @@ import com.google.gson.annotations.SerializedName
 
 data class Auth(
     @SerializedName("token")
-    val token: String
+    val token: String?,
+    @SerializedName("status")
+    val status: String?,
+    @SerializedName("msg")
+    val msg: String?
+) {
 
-)
+    fun isError() : Boolean {
+        return token == null || token.isEmpty()
+    }
+
+}
