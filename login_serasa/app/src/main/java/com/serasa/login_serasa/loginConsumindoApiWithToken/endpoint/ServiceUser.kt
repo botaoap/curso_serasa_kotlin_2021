5b3a9c1f82d3375f7fe64508a7ccc8969d17426b
botaoap <gabrielbotaoap@gmail.com>
2021-08-09 10:17:32 -0300

    Services Endpoints

--- login_serasa/app/src/main/java/com/serasa/login_serasa/loginConsumindoApiWithToken/endpoint/ServiceUser.kt
+++ login_serasa/app/src/main/java/com/serasa/login_serasa/loginConsumindoApiWithToken/endpoint/ServiceUser.kt
@@ -1,4 +1,18 @@
 package com.serasa.login_serasa.loginConsumindoApiWithToken.endpoint
 
+import android.net.Credentials
+import com.serasa.login_serasa.loginConsumindoApiWithToken.model.Auth
+import com.serasa.login_serasa.loginConsumindoApiWithToken.model.UserToken
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
 interface ServiceUser {
+
+    @GET("/users")
+    fun getAll(): Call<List<UserToken>>
+
+    @POST("/auth/login")
+    fun login(@Body credentials: Credentials): Call<Auth>
 }
