a16052297d18bcfd64332e49410747e981f9b98d
botaoap <gabrielbotaoap@gmail.com>
2021-08-09 10:22:02 -0300

    Tela de Login com validacoes basicas

--- login_serasa/app/src/main/java/com/serasa/login_serasa/loginConsumindoApiWithToken/ui/DetailUserTokenActivity.kt
+++ login_serasa/app/src/main/java/com/serasa/login_serasa/loginConsumindoApiWithToken/ui/DetailUserTokenActivity.kt
@@ -2,10 +2,19 @@ package com.serasa.login_serasa.loginConsumindoApiWithToken.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.serasa.login_serasa.R
 
 class DetailUserTokenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_user_token)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return super.onSupportNavigateUp()
+    }
+
 }
--- login_serasa/app/src/main/java/com/serasa/login_serasa/loginConsumindoApiWithToken/ui/LoginWithToken.kt
+++ login_serasa/app/src/main/java/com/serasa/login_serasa/loginConsumindoApiWithToken/ui/LoginWithToken.kt
@@ -1,19 +1,109 @@
 package com.serasa.login_serasa.loginConsumindoApiWithToken.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.serasa.login_serasa.R
 import com.serasa.login_serasa.loginConsumindoApi.model.User
 import com.serasa.login_serasa.loginConsumindoApi.serviceApi.ConnectionWithRetrofit
 import com.serasa.login_serasa.loginConsumindoApi.serviceApi.EndpointUser
+import com.serasa.login_serasa.loginConsumindoApiWithToken.endpoint.ServiceUser
+import com.serasa.login_serasa.loginConsumindoApiWithToken.extension.snackBar
+import com.serasa.login_serasa.loginConsumindoApiWithToken.model.Credentials
+import com.serasa.login_serasa.loginConsumindoApiWithToken.model.UserToken
+import com.serasa.login_serasa.loginConsumindoApiWithToken.serviceApi.ConnectionRetrofitWithToken
+import com.serasa.login_serasa.loginConsumindoApiWithToken.serviceApi.LinksApi
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class LoginWithToken : AppCompatActivity() {
+class LoginWithToken : AppCompatActivity(), Callback<List<UserToken>> {
+
+    private lateinit var inputUsername: EditText
+    private lateinit var inputPassword: EditText
+    private lateinit var buttonToken: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_with_token)
+
+        loadComponents()
+        actionToButtonLoginToken()
+        connectionRetrofitToApi()
+
+    }
+
+    fun actionToButtonLoginToken(){
+        buttonToken.setOnClickListener{
+            fazerLogin()
+
+        }
+    }
+
+    private fun fazerLogin() {
+        val username = inputUsername.text.toString()
+        val password = inputPassword.text.toString()
+
+        val credentirals = Credentials(username, password)
+
+        if (credentirals.checkUserName() && credentirals.checkPassword()) {
+            Intent(this, DetailUserTokenActivity::class.java).apply {
+                startActivity(this)
+            }
+        }else {
+            snackBar(inputUsername, R.string.usuario_invalido)
+        }
+    }
+
+    fun loadComponents() {
+        inputUsername = findViewById<EditText>(R.id.editTextUsernameWithToken)
+        inputPassword = findViewById<EditText>(R.id.editTextPasswordWithToken)
+        buttonToken = findViewById<Button>(R.id.buttonLoginUserWithToken)
+    }
+
+    fun connectionRetrofitToApi() {
+//        val getConnection =
+        ConnectionRetrofitWithToken
+            .getRetrofitInstance(LinksApi.FAKE_STORE_API.url)
+            .create(ServiceUser::class.java)
+            .getAll()
+            .clone().enqueue(this)
+
+//        val endpoint = getConnection.create(ServiceUser::class.java)
+//        val callback = endpoint.getAll()
+//
+//        callback.clone().enqueue(this)
+
+    }
+
+    override fun onResponse(
+        call: Call<List<UserToken>>,
+        response: Response<List<UserToken>>
+    ) {
+        println("Success Connection")
+//        val user = response.body()?.let { listOfUser ->
+//            listOfUser.filter {
+//                inputUsername.text.toString() == it.username
+//                inputPassword.text.toString() == it.password
+//            }
+//        }
+//        buttonToken.setOnClickListener {
+//            if (user != null) {
+//                Intent(this, DetailUserTokenActivity::class.java).apply {
+//                    startActivity(this)
+//                }
+//            } else {
+//                println("User don't exist")
+//            }
+//        }
+
+    }
+
+    override fun onFailure(call: Call<List<UserToken>>, t: Throwable) {
+        println("Failure Connection")
     }
 
 }
