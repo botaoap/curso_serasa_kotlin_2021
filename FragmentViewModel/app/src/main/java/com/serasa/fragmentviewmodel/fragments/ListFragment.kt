fb00edaa5f869bda3f7ea5057e5ff9cb9ae6faf9
botaoap <gabrielbotaoap@gmail.com>
2021-08-10 16:25:37 -0300

    Limpando Fragment

--- FragmentViewModel/app/src/main/java/com/serasa/fragmentviewmodel/fragments/ListFragment.kt
+++ FragmentViewModel/app/src/main/java/com/serasa/fragmentviewmodel/fragments/ListFragment.kt
@@ -22,27 +22,9 @@ private const val ARG_PARAM2 = "param2"
  */
 class ListFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
     private lateinit var showListWhatever: RecyclerView
     private lateinit var adapter: AdapterToFragment
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-
-    }
-    fun getInformationList(view: View) {
-        adapter = AdapterToFragment()
-
-        showListWhatever = view.findViewById(R.id.recyclerViewListWhatEver)
-        showListWhatever.layoutManager =
-            LinearLayoutManager(view.context, LinearLayoutManager.VERTICAL, false)
-        showListWhatever.adapter = adapter
-    }
+    
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -55,24 +37,14 @@ class ListFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         getInformationList(view)
     }
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
 
-            }
+    fun getInformationList(view: View) {
+        adapter = AdapterToFragment()
+
+        showListWhatever = view.findViewById(R.id.recyclerViewListWhatEver)
+        showListWhatever.layoutManager =
+            LinearLayoutManager(view.context, LinearLayoutManager.VERTICAL, false)
+        showListWhatever.adapter = adapter
     }
+
 }
