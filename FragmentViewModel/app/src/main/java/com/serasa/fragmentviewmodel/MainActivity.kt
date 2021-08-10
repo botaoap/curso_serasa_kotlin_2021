d6719de7bae29d10835ac4b6f6142f89c12e23c7
botaoap <gabrielbotaoap@gmail.com>
2021-08-10 15:01:54 -0300

    Replace Fragment with Button

--- FragmentViewModel/app/src/main/java/com/serasa/fragmentviewmodel/MainActivity.kt
+++ FragmentViewModel/app/src/main/java/com/serasa/fragmentviewmodel/MainActivity.kt
@@ -2,22 +2,40 @@ package com.serasa.fragmentviewmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.Fragment
 import com.serasa.fragmentviewmodel.fragments.PrimeiroFragment
 import com.serasa.fragmentviewmodel.fragments.SegundoFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private var tipoFragment = TypeFragment.PRIMEIRO
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container1, PrimeiroFragment())
-                .commitNow()
 
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container2, SegundoFragment())
-                .commitNow()
+        findViewById<Button>(R.id.buttonReplace).apply {
+            setOnClickListener {
+                if (tipoFragment == TypeFragment.PRIMEIRO) {
+                    replaceFrag(PrimeiroFragment())
+                    tipoFragment = TypeFragment.SEGUNDO
+                } else if (tipoFragment == TypeFragment.SEGUNDO) {
+                    replaceFrag(SegundoFragment())
+                    tipoFragment = TypeFragment.PRIMEIRO
+                }
+            }
         }
     }
+
+    private fun replaceFrag(fragment: Fragment){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, fragment)
+            .commitNow()
+    }
+}
+
+enum class TypeFragment {
+    PRIMEIRO,
+    SEGUNDO
 }
