0b8f3007a151d2895cb25aaadd7bde15213d0935
botaoap <gabrielbotaoap@gmail.com>
2021-08-11 16:58:43 -0300

    Gerenic Kotlin

--- FragmentViewModel/app/src/main/java/com/serasa/fragmentviewmodel/genericsFragment/runner/RunnerGenerics.kt
+++ FragmentViewModel/app/src/main/java/com/serasa/fragmentviewmodel/genericsFragment/runner/RunnerGenerics.kt
@@ -1,2 +1,13 @@
 package com.serasa.fragmentviewmodel.genericsFragment.runner
 
+fun main() {
+//    val cachorro: Cachorro()
+//    val camelo: Camelo()
+}
+
+
+class Camelo : Animal()
+
+class Cachorro : Animal()
+
+open class Animal
