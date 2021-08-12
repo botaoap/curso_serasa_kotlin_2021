80fe7dc42056f0d9d237f1db7c0c2186d1894b08
botaoap <gabrielbotaoap@gmail.com>
2021-08-12 13:16:58 -0300

    Shared Preferences

---
+++ projeto_shared_preferences/app/src/main/java/com/serasa/projeto_shared_preferences/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.serasa.projeto_shared_preferences
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.serasa.projeto_shared_preferences.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ projeto_shared_preferences/app/src/main/java/com/serasa/projeto_shared_preferences/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.serasa.projeto_shared_preferences.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
