2c4fbf732274b421352245c95ea960fe87767064
botaoap <gabrielbotaoap@gmail.com>
2021-08-16 15:40:59 -0300

    Get Response/Failure with ViewModel

--- architecture_mvvm/app/src/main/java/com/serasa/architecture_mvvm/view_model/MainViewModel.kt
+++ architecture_mvvm/app/src/main/java/com/serasa/architecture_mvvm/view_model/MainViewModel.kt
@@ -1,7 +1,32 @@
 package com.serasa.architecture_mvvm.view_model
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.serasa.architecture_mvvm.model.Article
+import com.serasa.architecture_mvvm.respository.ArticleRepository
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel : ViewModel(), Callback<List<Article>> {
+
+    private val _article = MutableLiveData<List<Article>>()
+    val articles: LiveData<List<Article>> = _article
+
+    private val repository = ArticleRepository()
+
+    fun fetchArticle() {
+        repository.getArticle(this)
+    }
+
+    override fun onResponse(call: Call<List<Article>>, response: Response<List<Article>>) {
+        response.body()?.apply {
+            _article.value = this
+        }
+    }
+
+    override fun onFailure(call: Call<List<Article>>, t: Throwable) {
+
+    }
 }
