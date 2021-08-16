3139077e77472822062abdbd9b897347753ae23f
botaoap <gabrielbotaoap@gmail.com>
2021-08-16 15:38:57 -0300

    Respository to Use Restrofit

--- architecture_mvvm/app/src/main/java/com/serasa/architecture_mvvm/respository/ArticleRepository.kt
+++ architecture_mvvm/app/src/main/java/com/serasa/architecture_mvvm/respository/ArticleRepository.kt
@@ -1,6 +1,16 @@
 package com.serasa.architecture_mvvm.respository
 
-class ProductRepository {
+import com.serasa.architecture_mvvm.model.Article
+import com.serasa.architecture_mvvm.services.RetrofitBuilder
+import retrofit2.Callback
 
 
+class ArticleRepository {
+
+    private val service = RetrofitBuilder.getArticleService()
+
+    fun getArticle(callBack : Callback<List<Article>>) {
+        val call = service.getArticle()
+        call.clone().enqueue(callBack)
+    }
 }
