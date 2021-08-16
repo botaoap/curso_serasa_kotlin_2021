0e3cda43d2a3deaa87802918afd0ec35bbe73e81
botaoap <gabrielbotaoap@gmail.com>
2021-08-16 15:38:27 -0300

    Connection API with Retrofit

--- architecture_mvvm/app/src/main/java/com/serasa/architecture_mvvm/services/RetrofitBuilder.kt
+++ architecture_mvvm/app/src/main/java/com/serasa/architecture_mvvm/services/RetrofitBuilder.kt
@@ -1,4 +1,15 @@
 package com.serasa.architecture_mvvm.services
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object RetrofitBuilder {
+    private val retrofitArticle = Retrofit.Builder()
+        .baseUrl("https://api.spaceflightnewsapi.net")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    fun getArticleService() : ArticleService {
+        return retrofitArticle.create(ArticleService::class.java)
+    }
 }
