22f9b940ae88a344627ae364c6bdc4f5fa796303
botaoap <gabrielbotaoap@gmail.com>
2021-08-16 15:39:57 -0300

    Call ViewModel, RecyclerView and Adapter to MainFragment

--- architecture_mvvm/app/src/main/java/com/serasa/architecture_mvvm/view/MainFragment.kt
+++ architecture_mvvm/app/src/main/java/com/serasa/architecture_mvvm/view/MainFragment.kt
@@ -1,34 +1,48 @@
 package com.serasa.architecture_mvvm.view
 
-import androidx.lifecycle.ViewModelProvider
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.serasa.architecture_mvvm.R
+import com.serasa.architecture_mvvm.adapter.AdapterArticle
+import com.serasa.architecture_mvvm.model.Article
 import com.serasa.architecture_mvvm.view_model.MainViewModel
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(R.layout.main_fragment) {
 
     companion object {
         fun newInstance() = MainFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+    private lateinit var viewmodel : MainViewModel
+    private lateinit var articleRecyclerView: RecyclerView
+    private var adapter = AdapterArticle {
 
     }
+
+    private val articleObserver = Observer<List<Article>> { newList ->
+        adapter.refresh(newList)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        articleRecyclerView = view.findViewById(R.id.recyclerViewArticle)
+        articleRecyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
+        articleRecyclerView.adapter = adapter
+
+        viewmodel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewmodel.articles.observe(viewLifecycleOwner, articleObserver)
+        viewmodel.fetchArticle()
+
+    }
+
 
 
 
