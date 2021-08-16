1c54fcdd368e5cc339ada518a913f0b065b8eedd
botaoap <gabrielbotaoap@gmail.com>
2021-08-16 15:38:08 -0300

    EndPoint to API

--- architecture_mvvm/app/src/main/java/com/serasa/architecture_mvvm/services/ArticleService.kt
+++ architecture_mvvm/app/src/main/java/com/serasa/architecture_mvvm/services/ArticleService.kt
@@ -1,4 +1,10 @@
 package com.serasa.architecture_mvvm.services
 
-interface ProdutctService {
+import com.serasa.architecture_mvvm.model.Article
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ArticleService {
+    @GET("/v3/articles")
+    fun getArticle() : Call<List<Article>>
 }
