8f8673fa1dea0bce9380291a19eafb4b2735345e
botaoap <gabrielbotaoap@gmail.com>
2021-08-17 17:22:48 -0300

    Build Fragment and ViewModel of Bill to show ListOfBill

--- firebase_with_mvvm/app/src/main/java/com/serasa/firebase_with_mvvm/view/ContentFragment.kt
+++ firebase_with_mvvm/app/src/main/java/com/serasa/firebase_with_mvvm/view/ContentFragment.kt
@@ -3,10 +3,14 @@ package com.serasa.firebase_with_mvvm.view
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.lifecycle.GeneratedAdapter
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.serasa.firebase_with_mvvm.R
+import com.serasa.firebase_with_mvvm.adapter.AdapterBill
+import com.serasa.firebase_with_mvvm.model.Bill
 import com.serasa.firebase_with_mvvm.view_model.ContentViewModel
 
 class ContentFragment : Fragment(R.layout.content_fragment) {
@@ -16,10 +20,34 @@ class ContentFragment : Fragment(R.layout.content_fragment) {
     }
 
     private lateinit var viewModel: ContentViewModel
+    private lateinit var listBilRecyclerView : RecyclerView
+    private val adapter = AdapterBill()
 
+    val observerBill = Observer<List<Bill>> {
+        adapter.refresh(it)
+    }
+
+    val observerError = Observer<String> {
+
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        listBilRecyclerView = view.findViewById(R.id.recyclerViewListBill)
+        listBilRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        listBilRecyclerView.adapter = adapter
+
+
+        viewModel = ViewModelProvider(this).get(ContentViewModel::class.java)
+
+        viewModel.bill.observe(viewLifecycleOwner, observerBill)
+        viewModel.error.observe(viewLifecycleOwner, observerError)
+
+        viewModel.fetchBill()
+
+
+
     }
 
 }
--- firebase_with_mvvm/app/src/main/java/com/serasa/firebase_with_mvvm/view_model/ContentViewModel.kt
+++ firebase_with_mvvm/app/src/main/java/com/serasa/firebase_with_mvvm/view_model/ContentViewModel.kt
@@ -1,7 +1,32 @@
 package com.serasa.firebase_with_mvvm.view_model
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.ktx.Firebase
+import com.serasa.firebase_with_mvvm.model.Bill
+import com.serasa.firebase_with_mvvm.repository.BillRespository
 
 class ContentViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val _bill = MutableLiveData<List<Bill>>()
+    var bill : LiveData<List<Bill>> = _bill
+
+    private val _error = MutableLiveData<String>()
+    var error : LiveData<String> = _error
+
+    private val billRepository = BillRespository()
+
+    fun fetchBill() {
+        billRepository.fetchBill { bills, error ->
+            if (error != null) {
+                _error.value = error
+            }else {
+                _bill.value = bills
+            }
+
+        }
+    }
+
+
 }
