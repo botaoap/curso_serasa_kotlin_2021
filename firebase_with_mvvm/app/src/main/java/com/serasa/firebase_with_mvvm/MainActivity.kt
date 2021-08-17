1b6958abb4401c67e1d4d488ab5c811b265da9c1
botaoap <gabrielbotaoap@gmail.com>
2021-08-17 08:48:04 -0300

    Firebase  with MVVM

---
+++ firebase_with_mvvm/app/src/main/java/com/serasa/firebase_with_mvvm/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.serasa.firebase_with_mvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.serasa.firebase_with_mvvm.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ firebase_with_mvvm/app/src/main/java/com/serasa/firebase_with_mvvm/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.serasa.firebase_with_mvvm.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
