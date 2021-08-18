e812d1ed8b022e38d0aebcc04d0c07c5b2e2af0d
botaoap <gabrielbotaoap@gmail.com>
2021-08-18 14:47:22 -0300

    Log out

--- exercise_firebase_mvvm/app/src/main/java/com/serasa/exercise_firebase_mvvm/view/MainFragment.kt
+++ exercise_firebase_mvvm/app/src/main/java/com/serasa/exercise_firebase_mvvm/view/MainFragment.kt
@@ -6,7 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.serasa.exercise_firebase_mvvm.R
+import com.serasa.exercise_firebase_mvvm.repository.AuthenticationRepository
+import com.serasa.exercise_firebase_mvvm.utils.replaceView
 import com.serasa.exercise_firebase_mvvm.view_model.MainViewModel
 
 class MainFragment : Fragment(R.layout.main_fragment) {
@@ -16,10 +19,16 @@ class MainFragment : Fragment(R.layout.main_fragment) {
     }
 
     private lateinit var viewModel: MainViewModel
-
+    private var auth = AuthenticationRepository()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+        view.findViewById<View>(R.id.imageViewArroBackToSignIn).setOnClickListener {
+            auth.signOut()
+            requireActivity().replaceView(SignInFragment.newInstance())
+        }
     }
 
 }
