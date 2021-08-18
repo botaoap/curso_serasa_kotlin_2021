844dfd9e5ef0f4da204697b4c8dd13f6e8fdf223
botaoap <gabrielbotaoap@gmail.com>
2021-08-18 11:42:07 -0300

    Method Sign Up on Firebase

--- exercise_firebase_mvvm/app/src/main/java/com/serasa/exercise_firebase_mvvm/view/SignUpFragment.kt
+++ exercise_firebase_mvvm/app/src/main/java/com/serasa/exercise_firebase_mvvm/view/SignUpFragment.kt
@@ -1,15 +1,19 @@
 package com.serasa.exercise_firebase_mvvm.view
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseUser
 import com.serasa.exercise_firebase_mvvm.R
+import com.serasa.exercise_firebase_mvvm.utils.replaceView
 import com.serasa.exercise_firebase_mvvm.view_model.SignUpViewModel
 
-class SignUpFragment : Fragment() {
+class SignUpFragment : Fragment(R.layout.sign_up_fragment) {
 
     companion object {
         fun newInstance() = SignUpFragment()
@@ -17,17 +21,32 @@ class SignUpFragment : Fragment() {
 
     private lateinit var viewModel: SignUpViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.sign_up_fragment, container, false)
+    private val observerNewUser = Observer<FirebaseUser?> {
+        Snackbar.make(requireView(), "Account created", Snackbar.LENGTH_LONG).show()
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
         viewModel = ViewModelProvider(this).get(SignUpViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        viewModel.user.observe(viewLifecycleOwner, observerNewUser)
+
+        view.findViewById<Button>(R.id.buttonSignUp).setOnClickListener {
+            val inputEmail = view.findViewById<EditText>(R.id.editTextInputEmailSignUp)
+            val inputPassword = view.findViewById<EditText>(R.id.editTextInputPasswordSignUp)
+
+            if (!inputEmail.text.isNullOrEmpty() && !inputPassword.text.isNullOrEmpty()) {
+                viewModel.createNewAccount(
+                    inputEmail.text.toString(),
+                    inputPassword.text.toString()
+                )
+            }
+        }
+
+        view.findViewById<View>(R.id.imageViewArrowBack).setOnClickListener {
+            requireActivity().replaceView(SignInFragment.newInstance())
+        }
     }
 
 }
--- exercise_firebase_mvvm/app/src/main/java/com/serasa/exercise_firebase_mvvm/view_model/SignUpViewModel.kt
+++ exercise_firebase_mvvm/app/src/main/java/com/serasa/exercise_firebase_mvvm/view_model/SignUpViewModel.kt
@@ -1,7 +1,25 @@
 package com.serasa.exercise_firebase_mvvm.view_model
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseUser
+import com.serasa.exercise_firebase_mvvm.repository.AuthenticationRepository
 
 class SignUpViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val _user = MutableLiveData<FirebaseUser?>()
+    var user : LiveData<FirebaseUser?> = _user
+
+    private val _error = MutableLiveData<String?>()
+    var error : LiveData<String?> = _error
+
+    private val authRespository = AuthenticationRepository()
+
+    fun createNewAccount(email: String, password: String) {
+        authRespository.createAccountWithEmailPassword(email,password) {
+            _user.value = it
+        }
+
+    }
 }
