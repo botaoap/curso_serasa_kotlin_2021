3c5821c011291ec4f47d9fb3ca2ee114a99d9b6f
botaoap <gabrielbotaoap@gmail.com>
2021-08-18 08:58:33 -0300

    Authenticantion to SignIn on Firebase

--- exercise_firebase_mvvm/app/src/main/java/com/serasa/exercise_firebase_mvvm/repository/AuthenticationRepository.kt
+++ exercise_firebase_mvvm/app/src/main/java/com/serasa/exercise_firebase_mvvm/repository/AuthenticationRepository.kt
@@ -1,5 +1,39 @@
 package com.serasa.exercise_firebase_mvvm.repository
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import javax.security.auth.callback.Callback
+
 class AuthenticationRepository {
+
+    /**
+     * Authenticantion Login With Firebase
+     */
+
+    private val auth = FirebaseAuth.getInstance()
+
+    fun signInWithEmailPassword(
+        email: String,
+        password: String,
+        callback: (FirebaseUser?, String?) -> Unit
+    ) {
+        val task = auth.signInWithEmailAndPassword(email,password)
+        task.addOnSuccessListener { authResult ->
+            authResult.user?.let {
+                if (authResult != null) {
+                    callback(authResult.user, null)
+                } else {
+                    callback(null, "Erro no Login")
+                }
+            }
+        }
+        task.addOnFailureListener {
+            callback(null, it.message)
+        }
+    }
+
+
 
 }
