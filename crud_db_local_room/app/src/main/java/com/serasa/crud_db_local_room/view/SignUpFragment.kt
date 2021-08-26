3de7d049fc29602fad5ff8c89dd4f0f52f9c9fbe
botaoap <gabrielbotaoap@gmail.com>
2021-08-26 10:25:11 -0300

    Successful Sign Up with Firebase

--- crud_db_local_room/app/src/main/java/com/serasa/crud_db_local_room/view/SignUpFragment.kt
+++ crud_db_local_room/app/src/main/java/com/serasa/crud_db_local_room/view/SignUpFragment.kt
@@ -3,31 +3,58 @@ package com.serasa.crud_db_local_room.view
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseUser
 import com.serasa.crud_db_local_room.R
+import com.serasa.crud_db_local_room.databinding.SignUpFragmentBinding
+import com.serasa.crud_db_local_room.utils.hideKeyboard
+import com.serasa.crud_db_local_room.utils.replaceView
 import com.serasa.crud_db_local_room.view_model.SignUpViewModel
 
-class SignUpFragment : Fragment() {
+class SignUpFragment : Fragment(R.layout.sign_up_fragment) {
 
     companion object {
         fun newInstance() = SignUpFragment()
     }
 
+    private lateinit var binding: SignUpFragmentBinding
     private lateinit var viewModel: SignUpViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.sign_up_fragment, container, false)
+    private val observerCreateUser = Observer<FirebaseUser> {
+        requireActivity().hideKeyboard()
+        Snackbar.make(requireView(), "Account created", Snackbar.LENGTH_LONG).show()
+        requireActivity().replaceView(SignInFragment.newInstance())
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    private val observerError = Observer<String> {
+        requireActivity().hideKeyboard()
+        Snackbar.make(requireView(), it, Snackbar.LENGTH_LONG).show()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = SignUpFragmentBinding.bind(view)
         viewModel = ViewModelProvider(this).get(SignUpViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        viewModel.user.observe(viewLifecycleOwner, observerCreateUser)
+        viewModel.error.observe(viewLifecycleOwner, observerError)
+
+        binding.buttonSignUp.setOnClickListener {
+            if (!binding.editTextInputEmailSignUp.text.isNullOrEmpty() &&
+                !binding.editTextInputPasswordSignUp.text.isNullOrEmpty()){
+                viewModel.createAccountWithEmailPassword(
+                    binding.editTextInputEmailSignUp.text.toString(),
+                    binding.editTextInputPasswordSignUp.text.toString()
+                )
+            }
+        }
+
+        binding.imageViewArrowBackSignUp.setOnClickListener {
+            requireActivity().replaceView(SignInFragment.newInstance())
+        }
     }
 
 }
--- crud_db_local_room/app/src/main/java/com/serasa/crud_db_local_room/view_model/SignUpViewModel.kt
+++ crud_db_local_room/app/src/main/java/com/serasa/crud_db_local_room/view_model/SignUpViewModel.kt
@@ -1,7 +1,26 @@
 package com.serasa.crud_db_local_room.view_model
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseUser
+import com.serasa.crud_db_local_room.repository.AuthenticantionRepository
 
 class SignUpViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val _user = MutableLiveData<FirebaseUser>()
+    var user: LiveData<FirebaseUser> = _user
+
+    private var _error = MutableLiveData<String>()
+    var error: LiveData<String> = _error
+
+    private val authRepository = AuthenticantionRepository()
+
+    fun createAccountWithEmailPassword(email: String, password: String) {
+        authRepository.creatAccountWithEmailPassword(email,password) { firebaseUser ->
+            if (firebaseUser != null) {
+                _user.value = firebaseUser
+            }
+        }
+    }
 }
