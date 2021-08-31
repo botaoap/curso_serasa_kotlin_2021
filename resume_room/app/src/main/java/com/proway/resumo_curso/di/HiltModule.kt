7c50de89a7c26efc88d03586813c139f0afb6c7c
botaoap <gabrielbotaoap@gmail.com>
2021-08-31 16:01:10 -0300

    Dependency Injection

--- resume_room/app/src/main/java/com/proway/resumo_curso/di/HiltModule.kt
+++ resume_room/app/src/main/java/com/proway/resumo_curso/di/HiltModule.kt
@@ -1,9 +1,15 @@
 package com.proway.resumo_curso.di
 
+import android.content.Context
+import com.proway.resumo_curso.database.AppDatabase
+import com.proway.resumo_curso.database.dao.GitHubDAO
 import com.proway.resumo_curso.repository.GithubRepository
+import com.proway.resumo_curso.service.GithubServices
+import com.proway.resumo_curso.service.RetrofitService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 
 
@@ -12,5 +18,16 @@ import dagger.hilt.components.SingletonComponent
 object HiltModule {
 
     @Provides
-    fun provideGitHubRepository() : GithubRepository = GithubRepository()
+    fun provideGitHubRepository(
+        dao: GitHubDAO,
+        services: GithubServices
+    ) : GithubRepository = GithubRepository(dao, services)
+
+    @Provides
+    fun provideGitHubServices(): GithubServices = RetrofitService.getGithubServices()
+
+    @Provides
+    fun provideGitHubDao(@ApplicationContext context: Context): GitHubDAO {
+        return AppDatabase.getDatabase(context).getGithubDao()
+    }
 }
