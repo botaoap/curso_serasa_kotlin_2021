d58fdb1a9440f5e3c89f54e93f33553e6794bbea
botaoap <gabrielbotaoap@gmail.com>
2021-09-01 09:44:34 -0300

    Using dependency injection to show recyclerview on screen

--- dependency_injection_users_github/app/src/main/java/com/serasa/dependency_injection_users_github/adapater/AdapterUser.kt
+++ dependency_injection_users_github/app/src/main/java/com/serasa/dependency_injection_users_github/adapater/AdapterUser.kt
@@ -1,4 +1,51 @@
 package com.serasa.dependency_injection_users_github.adapater
 
-class AdapterUser {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.serasa.dependency_injection_users_github.R
+import com.serasa.dependency_injection_users_github.databinding.ItemUsersBinding
+import com.serasa.dependency_injection_users_github.model.User
+
+class AdapterUser : RecyclerView.Adapter<ItemUserViewHolder>(){
+
+    private val listOfUser = mutableListOf<User>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemUserViewHolder {
+        LayoutInflater.from(parent.context).inflate(R.layout.item_users, parent, false).apply {
+            return ItemUserViewHolder(this)
+        }
+    }
+
+    override fun onBindViewHolder(holder: ItemUserViewHolder, position: Int) {
+        listOfUser[position].apply {
+            holder.bind(this)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return listOfUser.size
+    }
+
+    fun refresh(newlist : List<User>) {
+        listOfUser.addAll(newlist)
+        notifyDataSetChanged()
+    }
+}
+
+class ItemUserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private val binding = ItemUsersBinding.bind(itemView)
+
+    fun bind(user: User) {
+        binding.textViewIdUser.text = user.id.toString()
+        binding.textViewNameUser.text = user.login
+        binding.imageViewAvatarUser.apply {
+            Glide.with(itemView)
+                .load(user.image)
+                .into(this)
+        }
+
+    }
 }
--- dependency_injection_users_github/app/src/main/java/com/serasa/dependency_injection_users_github/view/MainFragment.kt
+++ dependency_injection_users_github/app/src/main/java/com/serasa/dependency_injection_users_github/view/MainFragment.kt
@@ -7,8 +7,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.GeneratedAdapter
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.serasa.dependency_injection_users_github.R
+import com.serasa.dependency_injection_users_github.adapater.AdapterUser
 import com.serasa.dependency_injection_users_github.databinding.MainFragmentBinding
 import com.serasa.dependency_injection_users_github.model.User
 import com.serasa.dependency_injection_users_github.view_model.MainViewModel
@@ -23,9 +27,11 @@ class MainFragment : Fragment(R.layout.main_fragment) {
 
     private lateinit var viewModel: MainViewModel
     private lateinit var binding: MainFragmentBinding
+    private lateinit var adapter: AdapterUser
+    private lateinit var recyclerView: RecyclerView
 
     private val observerUser = Observer<List<User>> {
-
+        adapter.refresh(it)
     }
     private val observerPage = Observer<Int> {
 
@@ -39,17 +45,26 @@ class MainFragment : Fragment(R.layout.main_fragment) {
         super.onViewCreated(view, savedInstanceState)
 
         loadComponents(view)
+        executeComponents()
     }
 
     fun loadComponents(view: View) {
         binding = MainFragmentBinding.bind(view)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        adapter = AdapterUser()
+        recyclerView = binding.recyclerViewUsers
 
         viewModel.user.observe(viewLifecycleOwner, observerUser)
         viewModel.page.observe(viewLifecycleOwner, observerPage)
         viewModel.error.observe(viewLifecycleOwner, observerError)
 
+    }
+
+    fun executeComponents() {
         viewModel.fetchUser()
+
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = adapter
 
     }
 
