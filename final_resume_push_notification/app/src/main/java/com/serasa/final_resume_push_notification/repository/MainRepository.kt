83e3e64c9475624a455d9c6b16baad5d7ba427d9
botaoap <gabrielbotaoap@gmail.com>
2021-09-17 10:03:45 -0300

    Exclude project

--- final_resume_push_notification/app/src/main/java/com/serasa/final_resume_push_notification/repository/MainRepository.kt
+++
@@ -1,4 +0,0 @@
-package com.serasa.final_resume_push_notification.repository
-
-class MainRepository {
-}
--- final_resume_push_notification/app/src/main/java/com/serasa/final_resume_push_notification/view_model/MainViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.serasa.final_resume_push_notification.view_model
-
-import androidx.lifecycle.ViewModel
-
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
